bb4869bb988147317b3b61aa6793d850b423946e
jmromanos <jmromanos96@gmail.com>
2017-12-20 21:02:45 +0100

    Format

--- team/src/main/java/urlshortener/blacklodge/accessor/CheckWordsAccesor.java
+++ team/src/main/java/urlshortener/blacklodge/accessor/CheckWordsAccesor.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name="CheckWordsAccesor",url="http://www.wdylike.appspot.com",configuration = checkWordsConfiguration.class,fallback = checkWordsError.class)
+@FeignClient(name="CheckWordsAccesor",url="http://www.wdylike.appspot.com",
+        configuration = checkWordsConfiguration.class,fallback = checkWordsError.class)
 public interface CheckWordsAccesor {
     @RequestMapping(method = RequestMethod.GET, path="/?q={word}",produces = "text/plain")
     Boolean check(@PathVariable("word") String word);
