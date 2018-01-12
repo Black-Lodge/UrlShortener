0fe2fb74add8d7c0186f9e99b9c94237bf4168a8
Xian Pan <xian_pan_123@hotmail.com>
2018-01-12 11:26:07 -0500

    Fixed scheduler

--- team/src/main/java/urlshortener/blacklodge/scheduler/SafeBrowsingScheduler.java
+++ team/src/main/java/urlshortener/blacklodge/scheduler/SafeBrowsingScheduler.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import urlshortener.blacklodge.model.UrlShortenerModelImpl;
@@ -12,6 +14,8 @@ import urlshortener.blacklodge.repository.ShortURLRepo;
 import urlshortener.blacklodge.services.SafeBrowsingService;
 import urlshortener.common.domain.ShortURL;
 
+@Configuration
+@EnableScheduling
 public class SafeBrowsingScheduler {
     private final static Logger logger = LoggerFactory.getLogger(SafeBrowsingScheduler.class);
     
@@ -21,16 +25,19 @@ public class SafeBrowsingScheduler {
     @Autowired
     ShortURLRepo shortUrlRepository;
     
-    @Scheduled(fixedDelay = 100000L)
+    @Scheduled(fixedDelay = 50000)
     public void checkSafe() {
+        logger.info("Check safe scheduler");
         List<ShortURL> shorturls = shortUrlRepository.findAll();
 
         boolean before;
         boolean after;
         
         for (ShortURL shorturl : shorturls) {
+                //logger.info(shorturl.getTarget());
                 before = shorturl.getSafe();
                 after = safeBrowsingService.checkSafetyUrl(shorturl.getTarget());
+                //logger.info(String.valueOf(after));
                 if (before != after) {
                     shorturl.setSafe(after);
                     shortUrlRepository.update(shorturl);
