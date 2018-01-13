bd61453af200a29bc80a2ad7b80bb13a1609cecd
jmromanos <jmromanos96@gmail.com>
2018-01-13 19:28:17 +0100

    Added comments to domain classes

--- team/src/main/java/urlshortener/blacklodge/domain/Data.java
+++ team/src/main/java/urlshortener/blacklodge/domain/Data.java
@@ -1,27 +1,30 @@
 package urlshortener.blacklodge.domain;
 
+/**
+ * Class that represents some data for the Imgflip API
+ */
 public class Data {
     private String url;
     private String page_url;
-    /**
+    /** Get url
      * @return the url
      */
     public String getUrl() {
         return url;
     }
-    /**
+    /** Set url
      * @param url the url to set
      */
     public void setUrl(String url) {
         this.url = url;
     }
-    /**
+    /** Get page_url
      * @return the page_url
      */
     public String getPage_url() {
         return page_url;
     }
-    /**
+    /** Set page_url
      * @param page_url the page_url to set
      */
     public void setPage_url(String page_url) {
--- team/src/main/java/urlshortener/blacklodge/domain/MemeImageResponse.java
+++ team/src/main/java/urlshortener/blacklodge/domain/MemeImageResponse.java
@@ -1,27 +1,30 @@
 package urlshortener.blacklodge.domain;
 
+/**
+ * Class that represents a response from the Imgflip API
+ */
 public class MemeImageResponse {
     private Boolean success;
     private Data data;
-    /**
+    /** Get success
      * @return the success
      */
     public Boolean getSuccess() {
         return success;
     }
-    /**
+    /** Set success
      * @param success the success to set
      */
     public void setSuccess(Boolean success) {
         this.success = success;
     }
-    /**
+    /** Get data
      * @return the data
      */
     public Data getData() {
         return data;
     }
-    /**
+    /** Set data
      * @param data the data to set
      */
     public void setData(Data data) {
