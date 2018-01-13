f0fe2d6cf69a549226e7c66b4079bf23e37ffdb4
jmromanos <jmromanos96@gmail.com>
2018-01-13 19:35:23 +0100

    Added comments to ads classes

--- team/src/main/java/urlshortener/blacklodge/ads/AdsResponse.java
+++ team/src/main/java/urlshortener/blacklodge/ads/AdsResponse.java
@@ -1,5 +1,8 @@
 package urlshortener.blacklodge.ads;
 
+/**
+ * Class that presents a response related to an ad
+ */
 public class AdsResponse {
 	
 	int countdown;
@@ -10,18 +13,34 @@ public class AdsResponse {
 		this.url = url;
 	}
 
+	/**
+	 * Get countdown
+	 * @return countdown
+	 */
 	public int getCountdown() {
 		return countdown;
 	}
 
+	/**
+	 * Set countdown
+	 * @param countdown countdown
+	 */
 	public void setCountdown(int countdown) {
 		this.countdown = countdown;
 	}
 
+	/**
+	 * Get Url
+	 * @return URL
+	 */
 	public String getUrl() {
 		return url;
 	}
 
+	/**
+	 * Set Url
+	 * @param url URL
+	 */
 	public void setUrl(String url) {
 		this.url = url;
 	}
