0d5fea444365b251cc8534c5608efe1ba74da68a
jmromanos <jmromanos96@gmail.com>
2018-01-14 14:56:32 +0100

    Added comments to meme generator tests

--- team/src/test/java/urlshortener/blacklodge/MemeGenerator/MemeImageGeneratorServiceTest.java
+++ team/src/test/java/urlshortener/blacklodge/MemeGenerator/MemeImageGeneratorServiceTest.java
@@ -2,6 +2,7 @@ package urlshortener.blacklodge.MemeGenerator;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,18 +13,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 import urlshortener.blacklodge.Application;
 import urlshortener.blacklodge.services.MemeImageGeneratorService;
 
+/**
+ * Tests that the Imgflip API works as expected
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class MemeImageGeneratorServiceTest {
     
     @Autowired
     MemeImageGeneratorService memeImageGeneratorService;
-    
+
+    /**
+     * Checks that a new image is generated, given a new order of the words
+     */
     @Test
     public void generateMemeImage() {
         String image_url = memeImageGeneratorService.generateImage("lodge", "black");
-        
-        // Service 
+        assertNotNull(image_url);
         assertNotEquals("https://i.imgflip.com/20tzw3.jpg", image_url);
     }
 }
