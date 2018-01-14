fbf5d71e2197416fb2dcae7f497b0be7837e8562
jmromanos <jmromanos96@gmail.com>
2018-01-14 16:22:24 +0100

    Added comments to availability tests

--- team/src/test/java/urlshortener/blacklodge/checkAvailability/checkAvailabilityServiceTest.java
+++ team/src/test/java/urlshortener/blacklodge/checkAvailability/checkAvailabilityServiceTest.java
@@ -11,23 +11,32 @@ import org.springframework.test.context.junit4.SpringRunner;
 import urlshortener.blacklodge.Application;
 import urlshortener.blacklodge.services.CheckAvailabilityService;
 
+/**
+ * Tests the Availability Service
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class checkAvailabilityServiceTest {
 
     @Autowired
     CheckAvailabilityService checkAvailabilityService;
-    
+
+    /**
+     * Checks if it works as intended with websites that are known to be online, both http and https
+     */
     @Test
-    public void checkSuccess() throws Exception {
+    public void checkSuccess() {
         boolean checkResult = checkAvailabilityService.check("https://moodle2.unizar.es/add");
         assertEquals(true,checkResult);
         checkResult = checkAvailabilityService.check("http://mashable.com/ ");
         assertEquals(true,checkResult);
     }
-    
+
+    /**
+     * Checks if it works as intended with websites that are known to be offline, both http and https
+     */
     @Test
-    public void checkFailure() throws Exception {
+    public void checkFailure() {
         boolean checkResult = checkAvailabilityService.check("http://examplefail.com");
         assertEquals(false,checkResult);
         checkResult = checkAvailabilityService.check("https://examplefailsecure.com");
