f6aa202a9af7eca824f273829b010ec57c14be05
jmromanos <jmromanos96@gmail.com>
2018-01-15 22:10:50 +0100

    Comments missing

--- team/src/main/java/urlshortener/blacklodge/metrics/DiferentUsers.java
+++ team/src/main/java/urlshortener/blacklodge/metrics/DiferentUsers.java
@@ -4,18 +4,33 @@ import java.util.HashSet;
 import java.util.Set;
 import org.springframework.stereotype.Component;
 
+/**
+ * Class that represents the different users of the system
+ */
 @Component
 public class DiferentUsers {
+
   private static Set<String> ips;
 
+  /**
+   * Default constructor
+   */
   public DiferentUsers() {
     ips = new HashSet<String>() ;
   }
 
+  /**
+   * Get the number of different users
+   * @return number of users
+   */
   public int getNumber() {
     return ips.size();
   }
 
+  /**
+   * Add a user in case it does not exists in the system
+   * @param word The user to be added
+   */
   public void add(String word) {
     ips.add(word);
   }
